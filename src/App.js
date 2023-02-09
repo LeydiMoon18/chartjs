53039a7b11f61ffe221ed2450bf9746caa2944e3
LeydiMoon18 <98631662+LeydiMoon18@users.noreply.github.com>
2023-02-09 17:49:20 -0600

    Cambios en Gráfica
    Solo se forma una grafica en react con chartjs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React from 'react';
+//import React from 'react';
+import { Line } from 'react-chartjs-2';
+import {Chart as chartjs, Title, Tooltip, LineElement, Legend, CategoryScale, LinearScale, PointElement} from 'chart.js';
+import { useState } from 'react';
+chartjs.register(
+  Title, Tooltip, LineElement, Legend, CategoryScale, LinearScale, PointElement
+);
+//import './App.css';
 
-import LineChart from './Components/LineChart';
-import './App.css';
-
-const App = () => {
+function App () {
+  const [data, setData]=useState({
+    labels:['DD/MM/YYYY', 'DD/MM/YYYY','DD/MM/YYYY'],
+    datasets:[
+      {
+        label:"Gráfica",
+        data:[10, 20, 15],
+        backgroundColor:'gold',
+        borderColor:'blue',
+        tension:0.4,
+        fill:true,
+        pointStyle:'rect',
+        pointBorderColor:'blue',
+        pointBackgroundColor:'gold',
+        showLine:true
+      }
+    ]
+  })
   return(
-    <div>
-      <LineChart />
-      hola
+    <div className='App' style={{width:'800px', height:'800px'}}>
+      <Line data={data}></Line>
     </div>
-  )
+  );
 }
 
 export default App
